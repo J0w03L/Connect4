187aa7d8b50483dc201f71d656db2c22661eb48d
J0w03L <git@j0w03l.me>
2024-11-07 21:10:52 +0000

    Initial commit: playable grid and recursive win checking

---
+++ Connect4/Game.cs
@@ -0,0 +1,239 @@
+using System;
+
+using Connect4Config;
+
+namespace Connect4Game
+{
+    class Game
+    {
+        public enum SlotState : int
+        {
+            EMPTY = 0,  // Playable slot; no counter is here.
+            RED,        // Playable slot; red counter is here.
+            YELLOW,     // Playable slot; yellow counter is here.
+            BLOCK       // Unplayable slot; prevents FindAdjacentCounters from indexing out-of-bounds.
+        }
+
+        public enum Team : int
+        {
+            RED = 0,
+            YELLOW
+        }
+
+        public enum Direction : int
+        {
+            VERTICAL = 0,   // Upper/Lower
+            HORIZONTAL,     // Left/Right
+            DIAGONAL_1,     // Lower-Left/Upper-Right
+            DIAGONAL_2,     // Upper-Left/Lower-Right
+            END             // Indicates the end of directions
+        }
+
+        // Grid size.
+        public const int WIDTH = 7, HEIGHT = 6;
+        // How long a line must be to win.
+        public const int WIN_LINE_LENGTH = 4;
+
+        // ANSI Formatting Codes.
+        const string FG_RED = "\x1b[0;4;31m", FG_YELLOW = "\x1b[0;33m", FG_WHITE = "\x1b[0;37m";
+        const string BG_WHITE = "\x1b[47m", BG_BLACK = "\x1b[40m";
+        const string US_WHITE = "\x1b[4;37m", US_BLACK = "\x1b[4;30m";
+
+        // What a slot looks like as empty/red/yellow/block. Block should never appear.
+        static readonly string[] SLOT_CHARS = { " ", $"{FG_RED}\u25CF", $"{FG_YELLOW}\u25CF", "!" };
+
+        // + 2 on each because we're padding the edges of the grid with BLOCK slots.
+        // It's easier to check for adjacent counters that way; we don't have to worry
+        // about indexing out-of-bounds.
+        public static SlotState[,] grid = new SlotState[WIDTH + 2, HEIGHT + 2];
+
+        // What X position has the user currently got selected?
+        static int selectedX = 1;
+        public static Team playerTeam = Team.YELLOW;
+
+        public static void Play()
+        {
+            ClearGrid();
+
+            while (true)
+            {
+                PrintGrid();
+
+                Console.Write('\n');
+                Console.WriteLine("SPACE : Play Counter");
+                if (Config.DEBUG) Console.WriteLine("B     : Play Opponent Counter (DEBUG)");
+                Console.WriteLine("LEFT  : Move Counter Left");
+                Console.WriteLine("RIGHT : Move Counter Right");
+
+                ConsoleKey key = Console.ReadKey().Key;
+                switch (key)
+                {
+                    case ConsoleKey.Spacebar:
+                    case ConsoleKey.B:
+                        // If we're not in debug mode, ignore the debug key.
+                        if (key == ConsoleKey.B && !Config.DEBUG) break;
+
+                        int nextY = GetNextYForX(selectedX);
+                        if (nextY == 0) break;
+
+                        // Set wherever the player just selected to their team, or to opponent team if they
+                        // played the opponent's turn instead.
+                        grid[selectedX, nextY] = playerTeam == (key != ConsoleKey.B ? Team.RED : Team.YELLOW)
+                                                               ? SlotState.RED : SlotState.YELLOW;
+
+                        // Check to see if placing this counter created any winning lines.
+                        if (CheckForLines(selectedX, nextY))
+                        {
+                            Console.WriteLine("You won!");
+                            return;
+                        }
+
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (selectedX != 1) selectedX--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (selectedX != WIDTH) selectedX++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static void ClearGrid()
+        {
+            for (int y = 0; y < HEIGHT + 2; y++)
+            {
+                for (int x = 0; x < WIDTH + 2; x++)
+                {
+                    grid[x, y] = (x == 0 || x == WIDTH + 1 || y == 0 || y == HEIGHT + 1) ? SlotState.BLOCK : SlotState.EMPTY;
+                    if (Config.DEBUG) Console.Write($"{(byte)grid[x, y]}{(x == WIDTH + 1 ? '\n' : ' ')}");
+                }
+            }
+        }
+
+        static void PrintGrid()
+        {
+            Console.Clear();
+
+            for (int y = 1; y < HEIGHT + 2; y++)
+            {
+                if (y == HEIGHT + 1) break;
+
+                Console.Write("\n|");
+
+                for (int x = 1; x < WIDTH + 1; x++)
+                {
+                    bool isNext = (x == selectedX && y == GetNextYForX(x));
+
+                    if (isNext)
+                        Console.Write($"{US_WHITE}{(playerTeam == Team.RED ? FG_RED : FG_YELLOW)}\u25CB{US_WHITE}{FG_WHITE}|");
+                    else
+                        Console.Write($"{US_WHITE}{SLOT_CHARS[(int)grid[x, y]]}{FG_WHITE}|");
+                }
+            }
+
+            Console.Write("\n");
+        }
+
+        static void TestGrid()
+        {
+            if (!Config.DEBUG) return;
+
+            PrintGrid();
+
+            grid[3, 6] = SlotState.RED;
+            grid[5, 6] = SlotState.YELLOW;
+            grid[5, 5] = SlotState.RED;
+            grid[6, 6] = SlotState.RED;
+            grid[6, 5] = SlotState.RED;
+            grid[6, 4] = SlotState.RED;
+            grid[6, 3] = SlotState.RED;
+            grid[6, 2] = SlotState.RED;
+            grid[6, 1] = SlotState.RED;
+
+            PrintGrid();
+
+            Console.WriteLine(GetNextYForX(2));
+            Console.WriteLine(GetNextYForX(3));
+            Console.WriteLine(GetNextYForX(4));
+            Console.WriteLine(GetNextYForX(5));
+            Console.WriteLine(GetNextYForX(6));
+        }
+
+        static int GetNextYForX(int x)
+        {
+            int y = HEIGHT + 1;
+
+            while (y != 0)
+            {
+                --y;
+                if (grid[x, y] == SlotState.EMPTY) break;
+            }
+
+            return y;
+        }
+
+        static bool CheckForLines(int x, int y)
+        {
+            Team team = grid[x, y] == SlotState.RED ? Team.RED : Team.YELLOW;
+
+            for (int d = 0; d < (int)Direction.END; d++)
+            {
+                if (Config.DEBUG) Console.WriteLine($"Checking {(Direction)d}...");
+
+                int counted = 1;
+                FindAdjacentCounters((Direction)d, team, -1, ref counted, x, y);
+
+                if (counted >= WIN_LINE_LENGTH) return true;
+            }
+
+            if (Config.DEBUG) Console.ReadKey();
+
+            return false;
+        }
+
+        static void FindAdjacentCounters(Direction dir, Team team, int checkDirIndex, ref int counted, int x, int y)
+        {
+            int[][] dirs;
+
+            switch (dir)
+            {
+                case Direction.VERTICAL:
+                    dirs = new int[][] { new int[] { x - 1, y }, new int[] { x + 1, y } };
+                    break;
+                case Direction.HORIZONTAL:
+                    dirs = new int[][] { new int[] { x, y - 1 }, new int[] { x, y + 1 } };
+                    break;
+                case Direction.DIAGONAL_1:
+                    dirs = new int[][] { new int[] { x - 1, y - 1 }, new int[] { x + 1, y + 1 } };
+                    break;
+                case Direction.DIAGONAL_2:
+                    dirs = new int[][] { new int[] { x - 1, y + 1 }, new int[] { x + 1, y - 1 } };
+                    break;
+                default:
+                    throw new Exception("Unknown DIRECTION!");
+                    break;
+            }
+
+            int nextX, nextY;
+
+            for (int curDirIndex = (checkDirIndex == -1 ? 0 : checkDirIndex); curDirIndex < 2; curDirIndex++)
+            {
+                nextX = dirs[curDirIndex][0];
+                nextY = dirs[curDirIndex][1];
+
+                if (grid[nextX, nextY] == (team == Team.RED ? SlotState.RED : SlotState.YELLOW))
+                {
+                    if (Config.DEBUG) Console.WriteLine($"Found adjacent counter at [{nextX}, {nextY}].");
+                        
+                    counted++;
+                    FindAdjacentCounters(dir, team, curDirIndex, ref counted, nextX, nextY);
+                }
+
+                if (checkDirIndex != -1) return;
+            }
+        }
+    }
+}
---
+++ Connect4/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using Connect4Game;
+
+namespace Connect4Config
+{
+    class Config
+    {
+        public const bool DEBUG = true;
+        //public const bool DEBUG = false;
+    }
+}
+
+namespace Connect4
+{
+    internal class Program
+    {
+        /** 
+         *  These API imports are needed to enable ANSI support on Windows 10's cmd.exe.
+         *  See the following for info:
+         *      https://superuser.com/a/1300251
+         *      https://learn.microsoft.com/en-us/previous-versions/dotnet/netframework-4.0/ac7ay120(v=vs.100)?redirectedfrom=MSDN
+         *      https://learn.microsoft.com/en-us/windows/console/setconsolemode
+         *      https://learn.microsoft.com/en-us/windows/console/getconsolemode
+         *      https://learn.microsoft.com/en-us/windows/console/getstdhandle
+         */
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetStdHandle(UInt32 nStdHandle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool SetConsoleMode(IntPtr hConsoleHandle, UInt32 dwMode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetConsoleMode(
+            IntPtr hConsoleHandle,
+            [MarshalAs(UnmanagedType.U4)] out UInt32 lpMode
+        );
+
+        const UInt32 STD_OUTPUT_HANDLE = unchecked ((UInt32)(-11));
+        const UInt32 ENABLE_PROCESSED_INPUT = 0x1;
+        const UInt32 ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x4;
+
+        static readonly IntPtr INVALID_HANDLE_VALUE = new IntPtr(-1);
+
+        static void Main(string[] args)
+        {
+            // Ensure that we have ANSI color support.
+            // Unfortunately, the only way to do this is through the Windows API.
+            // We first have to get a handle to our console's standard output.
+            IntPtr conHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+            // Get and clear Win32 error, if any.
+            Int32 winError = Marshal.GetLastWin32Error();
+            if (conHandle == INVALID_HANDLE_VALUE)
+            {
+                Console.WriteLine("Could not get stdout handle! (error code {0:X})\nPress any key to exit.", winError);
+                Console.ReadKey();
+                return;
+            }
+
+            // Grab our current console mode.
+            if (!GetConsoleMode(conHandle, out UInt32 curConsoleMode))
+            {
+                winError = Marshal.GetLastWin32Error();
+                Console.WriteLine("Could not get console mode! (error code {0:X})\nPress any key to exit.", winError);
+                Console.ReadKey();
+                return;
+            }
+
+            const UInt32 requiredMode = ENABLE_PROCESSED_INPUT | ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+
+            // See if we need to set the mode or not.
+            if ((curConsoleMode & requiredMode) != requiredMode)
+            {
+                // We do need to set it, so let's do that.
+                if (!SetConsoleMode(conHandle, curConsoleMode | requiredMode))
+                {
+                    winError = Marshal.GetLastWin32Error();
+                    Console.WriteLine("Could not set console mode! (error code: {0:X})\nPress any key to exit.", winError);
+                    Console.ReadKey();
+                    return;
+                }
+            }
+            
+            // Ensure that we can print Unicode characters.
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // We're ready; let's start the game!
+            while (true)
+            {
+                Game.Play();
+
+                while (true)
+                {
+                    Console.Write("\nPlay again? (Y/N): ");
+
+                    ConsoleKey key = Console.ReadKey().Key;
+
+                    if (key == ConsoleKey.N) return;
+                    if (key == ConsoleKey.Y) break;
+                }
+            }
+        }
+    }
+}
